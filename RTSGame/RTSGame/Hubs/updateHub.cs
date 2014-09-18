a85fdde75a085b4498c5d8ea175ec1906a7668ce
Nikunj Patel <nikspatel007@gmail.com>
2014-09-17 22:09:46 -0500

    Pushing first project with simple signalR setup and framwork for chatbox.  Use following link to navigate to Chat box.

---
+++ RTSGame/RTSGame/Hubs/updateHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RTSGame.Hubs
+{
+    public class updateHub : Hub
+    {
+        public void callServer(String msg)
+        {
+            Clients.All.callClient(msg);
+        }
+    }
+}
---
+++ RTSGame/RTSGame/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RTSGame.Startup))]
+
+namespace RTSGame
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
