c38e27de369bb98a130dc5745de6dcb4c233f157
lcwadle <lcwadle@longhornhealth.com>
2014-09-18 19:56:38 -0500

    Basic setup, map and player objects created.  Folder structure added for basic game objects.

---
+++ RTS/Assets/RTS/Constants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS
+{
+    // Area for static variables such as camera angle, camera height, etc
+    // All classes must be static
+    public static class Constants {
+
+    }
+}
