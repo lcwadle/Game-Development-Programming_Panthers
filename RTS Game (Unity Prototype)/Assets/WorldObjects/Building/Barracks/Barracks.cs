8c189bbd3b43c049b1836604bf8fcd2623caaeea
lcwadle <lcwadle@longhornhealth.com>
2014-10-19 17:14:47 -0500

    no message

---
+++ RTS Game (Unity Prototype)/Assets/WorldObjects/Building/Barracks/Barracks.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barracks : Building
+{
+
+    protected override void Start()
+    {
+        base.Start();
+        actions = new string[]
+		{
+			"Soldier"
+		};
+    }
+
+    public override void PerformAction(string actionToPerform)
+    {
+        base.PerformAction(actionToPerform);
+        CreateUnit(actionToPerform);
+    }
+}
