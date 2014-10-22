9f5453c5ba2b8d44194394305b3361326607afd0
Lowery91 <lowery91@gmail.com>
2014-10-21 22:18:46 -0400

    Uploaded current version of the game.

---
+++ Shooter Project/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+	public int attackDamage = 10;               // The amount of health taken away per attack.
+	GameObject player;                          // Reference to the player GameObject.
+	PlayerHealth playerHealth;                  // Reference to the player's health.
+	EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+	bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+	float timer;                                // Timer for counting up to the next attack.
+	Animator anim;
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent <Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent<EnemyHealth>();
+	}
+	
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the entering collider is the player...
+		if(other.gameObject == player)
+		{
+			playerInRange = true; // ... the player is in range.
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		// If the exiting collider is the player...
+		if(other.gameObject == player)
+		{
+			playerInRange = false; // ... the player is no longer in range.
+		}
+	}
+	
+	
+	void Update ()
+	{
+		timer += Time.deltaTime; // Add the time since Update was last called to the timer.
+		
+		// If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+		if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+		{
+			anim.Play("Attack"); // Play the attack animation.
+			Attack (); // ... attack.
+		}
+	}
+	
+	// A function that the animator controller uses to revert back to the running animation after attacking.
+	void Run()
+	{
+		anim.SetTrigger("Run");
+	}
+	
+	
+	void Attack ()
+	{
+		timer = 0f; // Reset the timer.
+		
+		// If the player has health to lose...
+		if(playerHealth.currentHealth > 0)
+		{
+			playerHealth.TakeDamage (attackDamage); // ... damage the player.
+		}
+	}
+}
---
+++ Shooter Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;                            // The amount of health the player starts the game with.
+	public int currentHealth;                                   // The current health the player has.
+	PlayerMovement playerMovement;                              // Reference to the player's movement.
+	PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+	bool isDead;                                                // Whether the player is dead.
+	bool damaged;                                               // True when the player gets damaged.
+	
+	
+	void Awake ()
+	{
+		playerMovement = GetComponent <PlayerMovement> ();
+		playerShooting = GetComponentInChildren <PlayerShooting> ();
+		currentHealth = startingHealth; // Set the initial health of the player.
+	}
+	
+	
+	void Update ()
+	{
+		damaged = false; // Reset the damaged flag.
+	}
+	
+	
+	public void TakeDamage (int amount)
+	{
+		damaged = true; // Set the damaged flag.
+		currentHealth -= amount; // Reduce the current health by the damage amount.
+		
+		// If the player has lost all it's health and the death flag hasn't been set yet...
+		if(currentHealth <= 0 && !isDead)
+		{
+			// ... it should die.
+			Death ();
+		}
+	}
+	
+	
+	void Death ()
+	{
+		isDead = true; // Set the death flag so this function won't be called again.
+		playerShooting.DisableEffects (); // Turn off any remaining shooting effects.
+		
+		// Turn off the movement and shooting scripts.
+		playerMovement.enabled = false;
+		playerShooting.enabled = false;
+	} 
+}
