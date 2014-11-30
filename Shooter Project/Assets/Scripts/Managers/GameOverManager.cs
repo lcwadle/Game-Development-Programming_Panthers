9b0ab39e7d5560ec3b81478586a500f5bafb4c92
Lowery91 <lowery91@gmail.com>
2014-11-30 17:05:29 -0500

    Added game over screen when the player dies.

--- Shooter Project/Assets/Scripts/Managers/GameOverManager.cs
+++ Shooter Project/Assets/Scripts/Managers/GameOverManager.cs
@@ -5,12 +5,16 @@ public class GameOverManager : MonoBehaviour {
 
 	public PlayerHealth playerHealth;       // Reference to the player's health.
 	public float restartDelay = 5f;         // Time to wait before restarting the level
+	public float gameOverDelay = 1f;
 	float restartTimer;                     // Timer to count up to restarting the level
+	GUITexture gameOver;
 	
 	
 	void Awake ()
 	{
 		playerHealth = gameObject.GetComponent <PlayerHealth> ();
+		gameOver = GameObject.Find("GameOver").guiTexture;
+		gameOver.enabled = false;
 	}
 	
 	
@@ -22,6 +26,10 @@ public class GameOverManager : MonoBehaviour {
 			restartTimer += Time.deltaTime; // .. increment a timer to count up to restarting.
 			
 			// .. if it reaches the restart delay...
+			if(restartTimer >= gameOverDelay)
+			{
+				gameOver.enabled = true;
+			}
 			if(restartTimer >= restartDelay)
 			{
 				Application.LoadLevel(Application.loadedLevel); // .. then reload the currently loaded level.
